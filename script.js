16088bd96dd68bf64f95b5c68d6ffeeee9145012
Koku294 <90713556+Koku294@users.noreply.github.com>
2024-12-25 22:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  // Check if the year is a leap year
+  if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+    return 366; // Leap year
+  } else {
+    return 365; // Non-leap year
+  }
+}
+let x=daysOfAYear(2000);
+console.log(x);
